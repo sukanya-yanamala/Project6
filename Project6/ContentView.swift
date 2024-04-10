a5eadc191a79875049c534a15bdbc4a64a1c79de
Sukanya Yanamala <ysukanya383@gmail.com>
2024-04-10 12:29:36 +0100

    Initial Commit

---
+++ Project6/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Project6
+//
+//  Created by Sukanya Yanamala on 08/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack(spacing: 20){
+                Text("Hello, world!")
+                Text("This is inside a stack")
+            }
+        VStack {
+            Text("First")
+            Text("Second")
+            Text("Third")
+                .frame(maxWidth: 500, maxHeight: 50)
+                    .foregroundStyle(.white)
+                    .background(.red.gradient)
+        }
+        .padding()
+        .background(.red)
+        
+        ZStack {
+            Color.blue
+                .frame(width: 200, height: 200)
+            Text("Hello, world!")
+//            Text("This is inside a stack")
+        }
+        .padding(50)
+        .ignoresSafeArea()
+        
+        Button("Delete selection", action: executeDelete)
+        Button("Delete selection", role: .destructive, action: executeDelete)
+        
+        VStack {
+            Button("Button 1") { }
+                .buttonStyle(.bordered)
+            
+            Button {
+                print("Button was tapped")
+            } label: {
+                Label("Edit", systemImage: "pencil")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+            }
+        }
+    }
+    func executeDelete() {
+           print("Now deleting…")
+       }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project6/Project6App.swift
@@ -0,0 +1,17 @@
+//
+//  Project6App.swift
+//  Project6
+//
+//  Created by Sukanya Yanamala on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
